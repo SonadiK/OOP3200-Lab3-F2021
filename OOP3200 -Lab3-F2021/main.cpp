78f67e29137faf3c849131a63b05f35841aa7022
Sonadi Kannangara <ksonadi@gmail.com>
2021-10-03 21:54:15 -0400

    Added more comments

--- OOP3200 -Lab3-F2021/main.cpp
+++ OOP3200 -Lab3-F2021/main.cpp
@@ -22,27 +22,30 @@ using namespace std;
 
 int main()
 {
+	//Calling the default constructor.
 	ExtendedWorkTicket testTicket1;
+	//Calling the parameterized constructor.
 	ExtendedWorkTicket testTicket2(1, "AMCE_123", 1, 7, 2014, "Password Reset", true);
 
 	std::cout << std::endl << "As Initialized: " << std::endl;
-
+	//Calling the overloaded ShowWorkTicket()
 	std::cout << testTicket1.ShowWorkTicket() << std::endl;
 	std::cout << testTicket2.ShowWorkTicket() << std::endl;
 
-
+	// Calling the overloaded SetWorkTicket().
 	if (!testTicket1.SetWorkTicket(2, "MACDONALD-001", 10, 3, 2012, "User cannot locate \'any\' key.", true))
 		std::cout << "\nErrors! No changes to the ticket made." << std::endl;
-
+	//output operator (ostream).
 	std::cout << std::endl << "Ticket 1: " << testTicket1 << std::endl;
 
+	// Calling the overloaded SetWorkTicket().
 	if (!testTicket2.SetWorkTicket(-1, "BLAGO-042", 13, 32, 11, "", false))
 		std::cout << "\nErrors! No changes to the ticket made." << std::endl;
-
+	//output operator (ostream).
 	std::cout << std::endl << "Ticket 2: " << testTicket2 << std::endl;
-
+	//calling the CloseTicket() mutator
 	testTicket2.CloseTicket();
-
+	//output operator (ostream).
 	std::cout << std::endl << "After Ticket 2 closed: " << testTicket2 << std::endl;
 
 	std::cout << std::endl << std::endl;
