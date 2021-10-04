31b418d82dae468fc31f6a00ae65e453525fee81
Sonadi Kannangara <ksonadi@gmail.com>
2021-10-03 21:34:02 -0400

    Added default and parameterized constructors to ExtendeWorkTicket class.

--- OOP3200 -Lab3-F2021/ExtendedWorkTicket.cpp
+++ OOP3200 -Lab3-F2021/ExtendedWorkTicket.cpp
@@ -1,4 +1,30 @@
 #include "ExtendedWorkTicket.h"
 #include<iostream>
 
+//Default constructor
+ExtendedWorkTicket::ExtendedWorkTicket()
+{
+	SetTicket(0);
+	SetClient("");
+	SetDate(1, 1, 2000);
+	SetDescription("");
+	setIsOpen(true);
+}
 
+//Parameterized constructor
+ExtendedWorkTicket::ExtendedWorkTicket(const int ticket_number, const std::string& client_id, const int day, const int month, const int year, const std::string& description, bool is_open)
+	:WorkTicket(ticket_number, client_id, day, month, year, description), m_IsOpen(is_open)
+{
+
+}
+
+//Definition 
+bool ExtendedWorkTicket::getIsOpen() const
+{
+	return m_IsOpen;
+}
+
+void ExtendedWorkTicket::setIsOpen(const bool value)
+{
+	m_IsOpen = true;
+}
--- OOP3200 -Lab3-F2021/ExtendedWorkTicket.h
+++ OOP3200 -Lab3-F2021/ExtendedWorkTicket.h
@@ -7,12 +7,19 @@
 class ExtendedWorkTicket : public WorkTicket
 {
 public:
-	
+	//Default constructor
+	ExtendedWorkTicket();
+	//Parameterized constructor
+	ExtendedWorkTicket(int ticket_number, const std::string& client_id, int day, int month, int year, const std::string& description, bool isOpen);
 
+	//Declaration - accessor
 	bool getIsOpen() const;
+	//Mutator
+	void setIsOpen(bool value);
 
 private:
-	
+	//Member variables
+	bool m_IsOpen;
 };
 
 #endif
